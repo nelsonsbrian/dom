4e206f65a11b79a91bb56f2ff5760c4554bf6b4c
Akjol Jaenbai and Brian Nelson <brian@>
2018-08-07 13:25:02 -0700

    add function to remove photos from dialouge.

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $("button#bark").click(function() {
+    $('ul#dog').prepend('<img class="ruff" src="img/dog.png" />');
+    $("ul#dog").prepend("<li>Bark!</li>");
+    $("img").click(function(){
+      $(this).remove();
+    });
+
+  });
+
+  $("button#meow").click(function() {
+    $('ul#cat').prepend('<img class="meow" src="img/cat.jpeg" />');
+    $("ul#cat").prepend("<li>Meow!</li>");
+
+    $("img").click(function(){
+      $(this).remove();
+    });
+  });
+});
+
+//   $(this).remove();
+// });
+// $("ul.ruff").children("li").first().click(function() {
+//   $(this).remove();
+// });
