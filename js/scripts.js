f8745a82016246430f121c7527415b916b2a0d3e
Akjol Jaenbai and Brian Nelson <brian@>
2018-08-07 16:06:37 -0700

    add styles

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,30 @@ $(document).ready(function() {
   $("button#bark").click(function() {
     $('ul#dog').prepend('<img class="ruff" src="img/dog.png" />');
     $("ul#dog").prepend("<li>Bark!</li>");
+    $('ul#cat').prepend('<img class="meow" src="img/cat.jpeg" />');
+    $("ul#cat").prepend("<li>meooww?</li>");
     $("img").click(function(){
       $(this).remove();
     });
-
   });
 
   $("button#meow").click(function() {
     $('ul#cat').prepend('<img class="meow" src="img/cat.jpeg" />');
     $("ul#cat").prepend("<li>Meow!</li>");
-
+    $('ul#dog').prepend('<img class="ruff" src="img/dog.png" />');
+    $("ul#dog").prepend("<li>Ruff?</li>");
     $("img").click(function(){
       $(this).remove();
     });
   });
+
+  $("ul#cat").click(function() {
+  });
+  $("img").click(function(){
+    $(this).remove();
+  });
+
+
 });
 
 //   $(this).remove();
